b1414b51cf865ffaa7f7c00dcc343f9d7252a311
Nived Km <nivedkm588@gmail.com>
2023-06-05 19:28:11 +0530

    changes made

--- src/CreateTournament.js
+++ src/CreateTournament.js
@@ -1,6 +1,9 @@
-import React from 'react'
-import "./CreateTournament.css"
+import React from "react";
+import "./CreateTournament.css";
 import { useForm } from "react-hook-form";
+import { closeCreateTournament } from "./features/createOrJoinSlice";
+import { useDispatch } from "react-redux";
+import CloseIcon from "@mui/icons-material/Close";
 
 function CreateTournament() {
   const {
@@ -8,12 +11,51 @@ function CreateTournament() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  
+  const dispatch = useDispatch();
+  const onSubmit = (formData) => {
+    console.log(formData);
+  };
   return (
-    <div className='createtournament'>
-      
+    <div className="createtournament">
+      <div className="createtournament-header">
+        <h3>Create New Tournament</h3>
+        <CloseIcon
+          className="createtournament-close"
+          onClick={() => dispatch(closeCreateTournament())}
+        />
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          name="trname"
+          placeholder="Tournament name"
+          {...register("trname", { required: true })}
+        />
+        {errors.trname && (
+          <p className="createtournament-error">Name is required!</p>
+        )}
+
+        <input
+          type="number"
+          name="slots"
+          placeholder="Slots"
+          {...register("slots", { required: true })}
+        />
+        {errors.slots && (
+          <p className="createtournament-error">Enter number of slots!</p>
+        )}
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          {...register("password", { required: true })}
+        />
+        {errors.password && (
+          <p className="createtournament-error">Password is required!</p>
+        )}
+      </form>
     </div>
-  )
+  );
 }
 
-export default CreateTournament
+export default CreateTournament;
